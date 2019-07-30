bcb45fff4eb0736fde2ac597e53061e5ea8a1bcb
Olivier <olivierchemla@gmail.com>
2019-07-30 14:55:35 +0200

    Set $primary color to : orange

---
+++ src/components/Welcome/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6">
+          <h2>Write your Resume with Postly©</h2>
+          <p>Simply enter your informations in the builder section and your</p>
+        </div>
+        <div className="col-md-6" />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
