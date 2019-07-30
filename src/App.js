eb6115e6b3cefd62a9f5d8294e5f691e202bacf1
Olivier <olivierchemla@gmail.com>
2019-07-30 14:54:01 +0200

    Adding React-Router & UserProvider

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
+// Provide a commun state to all components
+import { UserProvider } from "./components/Builder/UserContext";
+
+// Import Components
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Dashboard from "./components/Dashboard/Dashboard";
+import Home from "./components/Welcome/Home";
+import Builder from "./components/Builder/Builder";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <Header />
-      <div className="wrapper">
-        <Dashboard />
-      </div>
-      <Footer />
-    </div>
+    <Router>
+      <UserProvider>
+        <div className="App">
+          <Header />
+          <div className="wrapper">
+            <Route exact path="/" component={Home} />
+            <Route path="/builder" component={Builder} />
+          </div>
+          <Footer />
+        </div>
+      </UserProvider>
+    </Router>
   );
-}
+};
 
 export default App;
